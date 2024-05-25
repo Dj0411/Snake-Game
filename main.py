826d82f83c5fe816b25c1552615c2929fbbbb681
Divyansh <156494195+Dj0411@users.noreply.github.com>
2024-05-25 15:44:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ while game_on:
         food.food_location()
         scoreboard.increment()
         snake.len_increment()
-        print("Khaa GYa mota")
 
     if(snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280):
         scoreboard.end()
